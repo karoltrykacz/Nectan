78b103a9174280b68d3ff23bafbe9fa020aa9647
karoltrykacz <nectan@nectan.co>
2026-09-07 15:51:40 +0200

    Refactor

--- desktop/src/main.rs
+++ desktop/src/main.rs
@@ -22,7 +22,7 @@ async fn main() -> Result<(), slint::PlatformError> {
 
     state
         .sender()
-        .send(Message::TransferOffer {
+        .send(Message::TransferOfferMsg {
             transfer_id: Uuid::default(),
             tree,
         })
@@ -73,7 +73,7 @@ pub fn start_event_listener(w: &NectanWindow, state: NectanState) {
         while let Ok(msg) = event_rx.recv().await {
             tracing::info!("New app event: {msg:#?}");
             match msg {
-                Message::TransferOffer { transfer_id, tree } => {
+                Message::TransferOfferMsg { transfer_id, tree } => {
                     show_transfer_offer(&w, transfer_id, tree);
                 }
                 _ => {
@@ -208,120 +208,4 @@ pub fn handle_incoming_transfer_offer(w: &NectanWindow) {
             });
         });
     });
-}
-
-pub struct TransferOfferInner {
-    pub info: TransferOfferInfo<PathTree>,
-    sender_id: DeviceId,
-    sender_name: Username,
-    respond: tokio::sync::mpsc::Sender<MessagePayload>,
-}
-
-#[derive(Clone)]
-pub struct TransferOffer {
-    inner: Arc<tokio::sync::Mutex<Option<TransferOfferInner>>>,
-    window: Weak<MainWindow>,
-}
-
-/// Only one connection request is allowed at a time
-/// If some offer is pending, all incoming will be discarded
-impl TransferOffer {
-    fn new(window: Weak<MainWindow>) -> Self {
-        TransferOffer {
-            inner: Arc::new(tokio::sync::Mutex::new(None)),
-            window,
-        }
-    }
-
-    pub async fn take(&self) -> Option<TransferOfferInner> {
-        self.inner.lock().await.take()
-    }
-
-    pub async fn read(&self) -> tokio::sync::MutexGuard<'_, Option<TransferOfferInner>> {
-        self.inner.lock().await
-    }
-    pub async fn add(
-        &self,
-        info: TransferOfferInfo<PathTree>,
-        sender_name: Username,
-        sender_id: DeviceId,
-        respond: tokio::sync::mpsc::Sender<MessagePayload>,
-    ) {
-        let mut lock = self.inner.lock().await;
-        match *lock {
-            Some(_) => {
-                // Some offer is already shown to the user
-                // Automatically reject the offer
-                let _ = respond.send(MessagePayload::RejectTransfer).await;
-            }
-            None => {
-                // Prepare and show user the offer
-                self.show(
-                    sender_name.to_string(),
-                    info.transfer_name.clone(),
-                    info.entries_num,
-                    info.total_size,
-                    info.transfer_id,
-                    info.tree.get_depth_0(),
-                );
-                *lock = Some(TransferOfferInner {
-                    sender_id,
-                    sender_name,
-                    info,
-                    respond,
-                });
-            }
-        }
-    }
-    pub async fn respond_to_offer(&self, transfer_id: Uuid, accept: bool) {
-        let response = match accept {
-            true => MessagePayload::AcceptTransfer,
-            false => MessagePayload::RejectTransfer,
-        };
-        let mut lock = self.inner.lock().await;
-        // Consume the offer
-        if let Some(offer) = lock.take() {
-            let _ = offer.respond.send(response).await;
-        }
-    }
-
-    fn show(
-        &self,
-        from: String,
-        name: String,
-        total_entries: u64,
-        total_size: u64,
-        id: Uuid,
-        mut depth0: Vec<(PathBuf, bool)>,
-    ) {
-        let nodes: Vec<TreeNode> = depth0
-            .drain(..)
-            .map(|(path, is_file)| TreeNode {
-                depth: 0,
-                expanded: false,
-                is_file,
-                filename: path
-                    .file_name()
-                    .unwrap_or_default()
-                    .to_string_lossy()
-                    .to_string()
-                    .into(),
-                path: path.to_string_lossy().to_string().into(),
-            })
-            .collect();
-
-        let _ = self.window.upgrade_in_event_loop(move |w| {
-            let b = w.global::<IncomingTransferOfferBridge>();
-            let offer = IncomingTransferOffer {
-                from: from.into(),
-                transfer_id: id.to_string().into(),
-                total_size: format_bytes(total_size).into(),
-                transfer_name: name.into(),
-                total_entries: total_entries as i32,
-            };
-            b.set_nodes(ModelRc::new(VecModel::from(nodes)));
-            b.set_offer(offer);
-            b.set_is_open(true);
-        });
-    }
 }
